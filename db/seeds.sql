8cf8f8208262dde4ffe8c15e97138dd50c77994e
myanguyen98 <waddio98@gmail.com>
2017-10-01 13:51:51 -0600

    almost commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name,devoured,date_made) VALUES("Double Bacon Cheeseburger",false,NOW());
+INSERT INTO burgers (burger_name,devoured,date_made) VALUES("Portobello Chicken Burger",false,NOW());
+INSERT INTO burgers (burger_name,devoured,date_made) VALUES("Blackbean Veggie Burger",false,NOW());
+INSERT INTO burgers (burger_name,devoured,date_made) VALUES("Pulled Pork Pickle Burger",DEFAULT,NOW());
